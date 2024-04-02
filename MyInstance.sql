2c2559a6499485e7cf74d02db8bd945437c2b1db
bleseneralexander <113368267+bleseneralexander@users.noreply.github.com>
2024-04-01 20:54:57 -0500

    Update MyInstance.sql

--- MyInstance.sql
+++ MyInstance.sql
@@ -1,8 +1,10 @@
-MyInstance,9090
+MyInstance,localhost
 user, root
-local,127.0.0.1
+myHost,127.0.0 .0.1.
 --pidfile="pidfile.sql"
 --socket="/home/bigmutha/.local/state/wireplumber"
 --port="22"
 --shared-memory-base-name="loc"
---
+--general_log_file="general_log_file.txt"
+--log-bin[="log_bin_file.sql"]
+--slow_query_log_file="slow_query_log_file.txt"
